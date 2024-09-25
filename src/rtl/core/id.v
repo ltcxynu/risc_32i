7efe7a848f958a85dda2e07148d3f4bff4003dc3
ltcxynu <ltc_xynu@163.com>
2024-09-25 17:24:49 +0800

    fix bug in id(reg/wire), add id_ex(pipe) in project. sigh,damn,where is my job.

--- src/rtl/core/id.v
+++ src/rtl/core/id.v
@@ -1,5 +1,8 @@
 //仅帮助传递数据，提前取数，不帮助ex提前申请数据通路，ex自己申请
 //全组合逻辑，设计时虽然开了两op_o，但大部分时候不使用，不传递。因为读寄存器的数据已经传递了。
+`ifndef SIM
+`include "rv32i_defines.v"
+`endif
 module id(
 input wire rst,
 //from if_id
@@ -13,15 +16,15 @@ input wire[`RegBus] csr_rdata_i,
 //from ex
 input wire ex_jump_flag_i,
 //read regs
-output wire[`RegAddrBus] reg1_raddr_o,
-output wire[`RegAddrBus] reg2_raddr_o,
+output reg[`RegAddrBus] reg1_raddr_o,
+output reg[`RegAddrBus] reg2_raddr_o,
 //read csr reg
 output reg [`MemAddrBus] csr_raddr_o,
 //to ex
-output reg [`RegAddrBus]    op1_o,
-output reg [`RegAddrBus]    op2_o,
-output reg [`RegAddrBus]    op1_jump_o,
-output reg [`RegAddrBus]    op2_jump_o,
+output reg [`RegBus]    op1_o,
+output reg [`RegBus]    op2_o,
+output reg [`RegBus]    op1_jump_o,
+output reg [`RegBus]    op2_jump_o,
 output reg [`InstBus]       inst_o,
 output reg [`InstAddrBus]   inst_addr_o,
 output reg [`RegBus]        reg1_rdata_o,
@@ -59,6 +62,7 @@ wire [2:0]U_rd = inst_i[11:7];
 wire [19:0]U_imm = inst_i[31:12];
 // J类型 无条件跳转 opcode += imm
 wire [2:0]J_funct3 = inst_i[14:12];
+wire [4:0] J_rd = inst_i[11:7];
 wire [19:0]J_imm = {inst_i[31],inst_i[19:12],inst_i[20],inst_i[30:21]};
 //这里明确一个容易误会的地方，I是指立即数型指令，但包含立即数的指令不一定是I型，type_I也不一定包含所有I型指令，例如LB
 always@(*) begin
@@ -141,14 +145,14 @@ always@(*) begin
             case(R_funct7)
                 7'b000_0000:begin
                     case(R_funct3)
-                        INST_ADD_SUB,
-                        INST_SLL    ,
-                        INST_SLT    ,
-                        INST_SLTU   ,
-                        INST_XOR    ,
-                        INST_SRL    ,
-                        INST_OR     ,
-                        INST_AND    :begin
+                        `INST_ADD_SUB,
+                        `INST_SLL    ,
+                        `INST_SLT    ,
+                        `INST_SLTU   ,
+                        `INST_XOR    ,
+                        `INST_SRL    ,
+                        `INST_OR     ,
+                        `INST_AND    :begin
                             //rd = rs1 ? rs2;
                             //向寄存器读取
                             reg1_raddr_o    = R_rs1;
@@ -221,10 +225,10 @@ always@(*) begin
         end
         `INST_TYPE_L    :begin
             case(I_funct3)
-                `INST_LB 
-                `INST_LH 
-                `INST_LW 
-                `INST_LBU
+                `INST_LB ,
+                `INST_LH ,
+                `INST_LW ,
+                `INST_LBU,
                 `INST_LHU:begin
                     //rd = Mem[rs1+imm];
                     reg1_raddr_o    = I_rs1;
@@ -251,7 +255,7 @@ always@(*) begin
                 end
             endcase
         end
-        INST_TYPE_CSR   :begin //CSR寄存器这些我认为不应该属于常规六种 IRSJBU
+        `INST_TYPE_CSR   :begin //CSR寄存器这些我认为不应该属于常规六种 IRSJBU
             case(I_funct3)
                 `INST_CSRRW ,
                 `INST_CSRRS ,
@@ -278,7 +282,7 @@ always@(*) begin
                     csr_raddr_o     = I_imm;
                     csr_rdata_o     = csr_rdata_i;
                     //向ex传递,仅传递立即数类型，不传递寄存器，寄存器读出来过pp2会到ex里。不多此一举
-                    op1_o           = {27'h0,rs1}; //zimm
+                    op1_o           = {27'h0,I_rs1}; //zimm
                     op2_o           = I_rd;
                     op1_jump_o      = `ZeroWord;
                     op2_jump_o      = `ZeroWord;
@@ -298,6 +302,28 @@ always@(*) begin
                     reg2_rdata_o    = `ZeroWord;
                 end
             endcase
+        end
+        `INST_JAL:begin
+            reg1_raddr_o    = `ZeroReg;
+            reg2_raddr_o    = `ZeroReg;
+            //向ex传递,仅传递立即数类型，不传递寄存器，寄存器读出来过pp2会到ex里。不多此一举
+            op1_o           = J_rd;
+            op2_o           = 32'h4;
+            op1_jump_o      = inst_addr_i;
+            op2_jump_o      = { {11{J_imm[19]}}, J_imm,1'b0};
+            reg1_rdata_o    = `ZeroWord;
+            reg2_rdata_o    = `ZeroWord;
+        end
+        `INST_JALR:begin
+            reg1_raddr_o    = I_rs1;
+            reg2_raddr_o    = `ZeroReg;
+            //向ex传递,仅传递立即数类型，不传递寄存器，寄存器读出来过pp2会到ex里。不多此一举
+            op1_o           = I_rd;
+            op2_o           = 32'h4;
+            op1_jump_o      = reg1_rdata_i;
+            op2_jump_o      = { {11{J_imm[19]}}, J_imm,1'b0};
+            reg1_rdata_o    = reg1_rdata_i;
+            reg2_rdata_o    = `ZeroWord;
         end
         `INST_LUI,`INST_AUIPC :begin
             reg1_raddr_o    = `ZeroReg;
---
+++ src/rtl/core/id_ex.v
@@ -0,0 +1,150 @@
+`ifndef SIM
+`include "rv32i_defines.v"
+`endif
+//第二级流水
+//pc -> | FF |     |  id  |    | FF |      
+// |    | FF | ->  |  ram | -> | FF | 
+//rom-> | FF |     |  csr |    | FF |      
+module id_ex(
+// from id
+input wire [`RegBus]        op1_i,
+input wire [`RegBus]        op2_i,
+input wire [`RegBus]        op1_jump_i,
+input wire [`RegBus]        op2_jump_i,
+input wire [`InstBus]       inst_i,
+input wire [`InstAddrBus]   inst_addr_i,
+input wire [`RegBus]        reg1_rdata_i,
+input wire [`RegBus]        reg2_rdata_i,
+input wire [`RegBus]        csr_rdata_i,
+// to ex
+output reg [`RegBus]        op1_o,
+output reg [`RegBus]        op2_o,
+output reg [`RegBus]        op1_jump_o,
+output reg [`RegBus]        op2_jump_o,
+output reg [`InstBus]       inst_o,
+output reg [`InstAddrBus]   inst_addr_o,
+output reg [`RegBus]        reg1_rdata_o,
+output reg [`RegBus]        reg2_rdata_o,
+output reg [`RegBus]        csr_rdata_o,
+// halt
+input wire [`Hold_Flag_Bus] hold_flag_i
+);
+wire hold_en = (hold_flag_i <= `Hold_Id);
+
+wire [`InstBus] op1;
+assign op1_o = op1;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_op1(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (op1_i),
+    .qout   (op1)
+);
+
+wire [`InstBus] op2;
+assign op2_o = op2;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_op2(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (op2_i),
+    .qout   (op2)
+);
+
+wire [`InstBus] op1_jump;
+assign op1_jump_o = op1_jump;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_op1_jump(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (op1_jump_i),
+    .qout   (op1_jump)
+);
+
+wire [`InstBus] op2_jump;
+assign op2_jump_o = op2_jump;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_op2_jump(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (op2_jump_i),
+    .qout   (op2_jump)
+);
+
+wire [`InstBus] inst;
+assign inst_o = inst;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_inst(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`INST_NOP),
+    .din    (inst_i),
+    .qout   (inst)
+);
+
+wire [`InstAddrBus] inst_addr;
+assign inst_addr_o = inst_addr;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_inst_addr(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (inst_addr_i),
+    .qout   (inst_addr)
+);
+
+wire [`InstAddrBus] reg1_rdata;
+assign reg1_raddr_o = reg1_rdata;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_reg1_rdata(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (reg1_raddr_i),
+    .qout   (reg1_rdata)
+);
+
+wire [`InstAddrBus] reg2_rdata;
+assign reg2_raddr_o = reg2_rdata;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_reg2_rdata(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (reg2_raddr_i),
+    .qout   (reg2_rdata)
+);
+
+wire [`InstAddrBus] csr_rdata;
+assign csr_rdata_o = csr_rdata;
+gen_pipe_dff #(
+    .DW(32)
+)pipe_csr_rdata(
+    .clk    (clk),
+    .rst    (rst),
+    .hold_en(HoldEnable),
+    .def_val(`ZeroWord),
+    .din    (csr_rdata_i),
+    .qout   (csr_rdata)
+);
+endmodule
